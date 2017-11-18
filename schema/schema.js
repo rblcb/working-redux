372d436b9b16e69b4cc869bd3ebc506fa25e2f2b
superdev610 <liaodingying610@hotmail.com>
2017-11-18 12:08:22 +0800

    circular references

--- schema/schema.js
+++ schema/schema.js
@@ -6,21 +6,29 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLList
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
     name: 'Company',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
-        description: { type: GraphQLString }
-    }
+        description: { type: GraphQLString },
+        user: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args){
+                return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+                .then(resp => resp.data);
+            }
+        }
+    })
 });
 
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         firstName: { type:GraphQLString },
         age: { type:GraphQLInt },
@@ -31,7 +39,7 @@ const UserType = new GraphQLObjectType({
                 .then(res => res.data);
             }
         }
-    }
+    })
 });
 
 const RootQuery = new GraphQLObjectType({
